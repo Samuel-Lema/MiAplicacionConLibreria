6933d21c9a08b79f638d0d9921eaea9cd1f055a3
Damian <damian@danielcastelao.org>
2018-01-24 18:46:58 +0100

    inicio de aplicación para añadir libreria

---
+++ src/com/dam1/miaplicacion/MiAplicacion.java
@@ -0,0 +1,17 @@
+package com.dam1.miaplicacion;
+
+import com.otros.milibreria.MisMetodos;
+/**
+ *
+ * @author damian
+ */
+public class MiAplicacion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println(MisMetodos.unMetodo(0));
+    }
+    
+}
